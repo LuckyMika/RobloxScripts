511c06c14f3d865dbe78fba1b0434e4e1fd3ec14
Mikachu <61615177+ShortMika@users.noreply.github.com>
2022-03-08 17:05:02 +0100

    Script for generic obbys
    Use this in generic obbys. You can config cpFolder by just setting it to the path of the Checkpoint Folder in game and the level to the leaderstats path.

---
+++ default obby script.lua
@@ -0,0 +1,46 @@
+-- Author: LuckyMika#6727
+-- Usage: place:
+--
+--              local cpFolder = game.Workspace.Checkpoints
+--              local level = game.Players.LocalPlayer.leaderstats.Stage
+--
+-- in your script and then load the following code with loadstring.
+--
+-- If you need more help, message me on discord.
+
+local cpFolder = game.Workspace.Checkpoints
+local level = game.Players.LocalPlayer.leaderstats.Stage
+if cpFolder == nil and level == nil then
+    game.Players.LocalPlayer:Kick("You didn't provide the Checkpoint Folder or Level")
+end
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Obby Gui", "Sentinel")
+local Tab = Window:NewTab("Teleportation")
+local Section = Tab:NewSection("Wanna see the end?")
+
+getgenv().autoFarm = false
+
+Section:NewButton("Next Level", "Teleports you to the next level", function ()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").RestartPortal.Portal.CFrame
+        wait()
+        local Event = game:GetService("ReplicatedStorage").Remotes.ToServer.RestartPlayer
+        Event:FireServer()
+        local newLevel = level.Value + 1
+        local cp = cpFolder[newLevel]
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = cp.CFrame * CFrame.new(0,3,0)
+end)
+
+Section:NewToggle("Auto Complete", "Auto completes all the levels for you", function (b)
+    getgenv().autoFarm = b
+    spawn(autoFarm)
+end)
+
+function autoFarm()
+    while getgenv().autoFarm do
+            local newLevel = level.Value + 1
+            local cp = cpFolder[newLevel]
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = cp.CFrame * CFrame.new(0,3,0)
+            wait(0.5)
+    end
+end
